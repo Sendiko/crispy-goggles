b128667aca0962c27ea87f51a8148e0235b52b99
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-10-15 07:34:46 +0700

    basic routing and http method

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+app.get('/users', (req, res) => {
+    res.send('Got all user data')
+})
+
+app.post('/users', (req, res) => {
+    res.send('Got a post request at /users')
+})
+
+app.put('/users/:id', (req, res) => {
+    res.send('Got a put request at /users')
+})
+
+app.delete('/users/:id', (req, res) => {
+    res.send('Got a delete request at /users')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at ${port}`)
 })
